da214d525a13bc34d7c9675ba6f89cceaec856bc
dasar <wanjikuestivah@gmail.com>
2024-10-12 17:58:21 +0400

    implementation of index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "../src/style.css";
+import { noteModal } from "./functions/noteModule";
+import { taskModal } from "./functions/taskModule";
+import { projectModal } from "./functions/projectModule";
+
+
+function initialize() {
+    noteModal();
+    taskModal();
+    projectModal();
+}
+
+document.addEventListener("DOMContentLoaded", initialize);
