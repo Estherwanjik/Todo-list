a6ff6225efc280cddc9ed97601f1838cba126da8
dasar <wanjikuestivah@gmail.com>
2024-10-18 16:06:09 +0400

    intriduced webpack merge and webpack.common.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+/* const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -26,4 +26,4 @@ module.exports = {
     ],
   },
 
-};
+}; */
