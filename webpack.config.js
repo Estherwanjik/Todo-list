c2f17ac8a949c86da7a2e26c10cf948cefe0befb
dasar <wanjikuestivah@gmail.com>
2024-10-28 11:33:11 +0400

    cleaned  the code

--- webpack.config.js
+++ webpack.config.js
@@ -10,21 +10,11 @@ module.exports = {
     clean: true,
   },
 
-  devServer: {
-    port: 8080,
-    // contentBase: path.resolve(__dirname, 'dist')
-  },
- /*  devtool: "eval-source-map",
+  devtool: "eval-source-map",
   devServer: {
   watchFiles: ["./src/index.html"],
-   
-  },  */
-  /* devServer: {
-    hot: true,
-    inline: true,
-  }, */
-
-
+  },
+ 
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
@@ -36,10 +26,6 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
-      /* {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      }, */
     ],
   },
 };
